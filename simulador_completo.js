427b9e86c0932d6f4127345c57211a841f59cd15
GaloRamiro <g_cisneros_23@hotmail.es>
2026-05-04 11:43:37 -0500

    Agrega la clase activa simula la navegacion en una sola página

--- simulador_completo.js
+++ simulador_completo.js
@@ -1,23 +1,30 @@
+let clientes = [];
+let creditos = [];
 
-  let clientes = [];
-  let creditos = [];
+let tasaInteres = 15;
+let clienteSeleccionado = null;
+let cuotaCalculada = 0;
+let montoCalculado = 0;
+let plazoCalculado = 0;
+let creditoAprobado = false;
 
-  let tasaInteres = 15;
-  let clienteSeleccionado = null;
-  let cuotaCalculada = 0;
-  let montoCalculado = 0;
-  let plazoCalculado = 0;
-  let creditoAprobado = false;
-
-  
 //Para recuperar o mostrar información usar los métodos de la clase utilitarios, puede agregar métodos adicionales en utilitarios
 
-function mostrarSeccion(id) {
+
+
+// Crear función ocultarSecciones()
+//No recibe parámetros
+//Debe quitar la clase activa a todas las secciones
+
+function ocultarSecciones() {
   let secciones = document.querySelectorAll("section");
 
   for (let i = 0; i < secciones.length; i++) {
     secciones[i].classList.remove("activa");
   }
+}
 
-  document.getElementById(id).classList.add("activa");
+function mostrarSeccion(id) {
+  ocultarSecciones(); // 1. ocultar todo
+  document.getElementById(id).classList.add("activa"); // 2. mostrar una
 }
